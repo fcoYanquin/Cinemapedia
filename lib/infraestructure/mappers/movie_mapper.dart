5e14f40c64d9d8380a5f530b2cdfddaa3e0cae9a
Francisco Yanquin Morales <javierynq@gmail.com>
2023-10-06 05:41:34 -0300

    update

--- lib/infraestructure/mappers/movie_mapper.dart
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infraestructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,5 +17,23 @@ class MovieMapper {
       title: moviedb.title,
       video: moviedb.video,
       voteAverage: moviedb.voteAverage,
-      voteCount: moviedb.voteCount);
+      voteCount: moviedb.voteCount
+  );
+
+  static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
+      adult: moviedb.adult,
+      backdropPath: moviedb.backdropPath != '' ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}' : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/800px-Image_not_available.png?20210219185637',
+      genreIds: moviedb.genres.map((e) => e.name).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: moviedb.posterPath != '' ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}' : 'no-poster',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount
+  );
 }
---
+++ lib/presentation/providers/actors/actors_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:cinemapedia/infraestructure/datasources/actor_moviedb_datasource.dart';
+import 'package:cinemapedia/infraestructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infraestructure/repositories/actor_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorRepositoryProvider = Provider((ref) {
+  return ActorRepositoryImpl(ActorMovieDbDatasource());
+});
